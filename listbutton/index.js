b93a4d3b1d3e31338873938e7d100acba3e0dc29
lcromartie <43760922+lcromartie@users.noreply.github.com>
2018-10-17 11:02:36 -0700

    Create index.js

---
+++ listbutton/index.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+const userInput = document.getElementById('list-data');
+const setTextButton = document.getElementById('st-text'); 
+} 
+setTextButton.onclick = () => {
+    userInput.value = 'The first item for my list';
