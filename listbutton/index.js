1b7dc8cb6500424365b04be7c1fb531321d749ae
lcromartie <43760922+lcromartie@users.noreply.github.com>
2018-10-19 00:39:32 -0700

    Add files via upload

---
+++ listbutton/index.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+const userInput = document.getElementById('list-data');
+const setTextButton = document.getElementById('st-text'); 
+} 
+setTextButton.onclick = () => {
+    userInput.value = 'The first item for my list';
+}
