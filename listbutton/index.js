82d40a807edf72da9aec7c0b8881e8d5ad132cca
lcromartie <lyndol.cromartie@bellevuecollege.edu>
2018-11-02 00:33:36 -0700

    trying to get the buttons to function.

--- listbutton/index.js
+++ listbutton/index.js
@@ -1,7 +1,14 @@
 window.onload = () => {
 const userInput = document.getElementById('list-data');
-const setTextButton = document.getElementById('st-text'); 
-} 
+const setTextButton = document.getElementById('set-text');
+const listbutton = document.getElementById('add-to-list'); 
+
 setTextButton.onclick = () => {
     userInput.value = 'The first item for my list';
+    listButton.onclick = () => {
+        const myList = document.getElementById('add-to-list');
+        elfCode.appendToList(myList, userInput.value);
+    }
+ }
 }
+
