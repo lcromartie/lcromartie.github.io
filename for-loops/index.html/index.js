003d0caec6bff86891017ba27e8db1dfcae09921
lcromartie <lyndol.cromartie@bellevuecollege.edu>
2018-11-06 23:24:43 -0800

    for-loop assignment

---
+++ for-loops/index.html/index.js
@@ -0,0 +1,8 @@
+windows.onload = () =>{
+    const listButton = document.getElementById('run-for-loop');
+
+    listButton.onclick = () =>{
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, i);
+    }
+}
