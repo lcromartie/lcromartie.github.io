c5931bfdad487bdf94863eab6015418493e93fbb
Lyndol Cromartie <lyndol.cromartie@bellevuecollege.edu>
2018-11-14 11:55:49 -0800

    changes to object-choices

--- javascriptTest/object-choices.js
+++ javascriptTest/object-choices.js
@@ -1,11 +1,24 @@
 function loader() {
     const getNamesButton = document.getElementById('get-name');
+    const functionButton = document.getElementById('getfunctionObject');
 
     getNamesButton.onclick = function() {
-        return this.firstName
+        const name = myObject.getName();
+        const getNameDisplay = document.getElementById('getNameDisplay');
+        getNameDisplay.textContent = name;
+
+        const fullName = myObject.getFullName();
+        const getFullNameDisplay = document.getElementById('getFullNameDisplay');
+        getFullNameDisplay.textContent = fullName;
+
+
     }
 }
-
+ 
+window.onload = loader;
+ 
+//Key value pairs
+//Coma
 const myObject = {
     firstName: 'fred',
     lastName: 'smith',
@@ -23,3 +36,13 @@ console.log(myObject.firstName);
 console.log(myObject['firstName']);
 console.log(myObject.getName());
 console.log(myObject.middleName);
+
+const funtionObject = () => {
+    console.log('my function object');
+
+    const getName = () => {
+        return 'Function object';
+    }
+
+    console.log(getName());
+}
